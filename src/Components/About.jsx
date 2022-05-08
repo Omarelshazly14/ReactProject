5ea85873aaabfc1b3ceb7416c884adf3b2635adf
Omarelshazly14 <omarelshazly014@gmail.com>
2022-05-08 09:21:29 +0200

    Adding About&Error

---
+++ src/Components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="alert alert-info w-75 m-auto text-center">About Us</div>
+  );
+}
---
+++ src/Components/Error.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Error() {
+  return (
+    <div className="alert alert-danger w-75 m-auto text-center">
+      Page Not Found!
+    </div>
+  );
+}
