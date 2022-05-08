33fff687d50adee17af299319b8decb2e57721c4
Omarelshazly14 <omarelshazly014@gmail.com>
2022-05-08 09:22:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header";
-// import { About } from "./Components/";
-// import { Error } from "./Components/";
+import { About } from "./Components/";
+import { Error } from "./Components/";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Posts from "./Components/Posts";
 import Slider from "./Components/Slider";
@@ -22,8 +21,8 @@ function App() {
         <Route path="/home" element={<Posts />} />
         <Route path="/home/details/:id" element={<Details />} />
         <Route path="/home/form" element={<Form />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="*" element={<Error />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
       </Routes>
 
       <Footer></Footer>
